b73dc3ba0ec2d4b2b7b08d46ef4d30a2f7ab9e8c
John Caraballo <jcaraballo019@gmail.com>
2022-09-19 09:32:34 -0400

    mint function

--- contracts/CoveyToken.sol
+++ contracts/CoveyToken.sol
@@ -3,9 +3,17 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract CoveyToken is ERC20 {
-  constructor(uint initialSupply) ERC20("Covey", "CVY")  {
-    _mint(msg.sender, initialSupply);
+contract CoveyToken is ERC20, Ownable {
+  uint256 public constant _maxSupply = 1_000_000 * 1e18;
+
+  constructor(uint initialSupply) ERC20("Covey", "CVY")  {}
+
+  function mint(
+    address account,
+    uint256 amount) public onlyOwner {
+      require(totalSupply() + amount <= _maxSupply, "Token Minting cannot exceed Max Supply");
+      _mint(account, amount);
   }
 }
