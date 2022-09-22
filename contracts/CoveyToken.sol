b2ca6b7812c9a68f22593cf71ad9c64ff1d85409
John Caraballo <jcaraballo019@gmail.com>
2022-09-22 14:59:57 -0400

    make token burnable

--- contracts/CoveyToken.sol
+++ contracts/CoveyToken.sol
@@ -2,10 +2,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract CoveyToken is ERC20, Ownable {
+
+contract CoveyToken is ERC20Burnable, Ownable {
   uint256 public constant _maxSupply = 1_000_000 * 1e18;
 
   constructor() ERC20("Covey", "CVY")  {}
@@ -16,4 +17,5 @@ contract CoveyToken is ERC20, Ownable {
       require(totalSupply() + amount <= _maxSupply, "Token Minting cannot exceed Max Supply");
       _mint(account, amount);
   }
+
 }
