9c45c5d8f30638cbfd08a7b36e68e9db95f08bd4
John Caraballo <jcaraballo019@gmail.com>
2022-09-22 15:53:23 -0400

    deploy to goerli with burnable

--- contracts/CoveyToken.sol
+++ contracts/CoveyToken.sol
@@ -1,12 +1,13 @@
 // contracts/CoveyToken.sol
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 
-contract CoveyToken is ERC20Burnable, Ownable {
+contract CoveyToken is ERC20, ERC20Burnable, Ownable {
   uint256 public constant _maxSupply = 1_000_000 * 1e18;
 
   constructor() ERC20("Covey", "CVY")  {}
